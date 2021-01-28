04b418c95b73998e73ec1833d5c1c4b8b6fe61eb
0/0 <imaskeleton@skulltown.net>
2021-01-28 12:37:56 -0700

    finish switching updater backend

--- utils/autoupdater.py
+++ utils/autoupdater.py
@@ -7,13 +7,20 @@ from utils import embeds
 from utils.ctx_class import MyContext
 
 
-async def continent(ctx: MyContext, country: str):
-    embed = await embeds.advanced_stats_embed(await ctx.bot.worldometers_api.try_to_get_name(country),
+async def continent(ctx: MyContext, name: str):
+    embed = await embeds.advanced_stats_embed(await ctx.bot.worldometers_api.try_to_get_name(name),
                                               ctx=ctx)
     if embed is None:
         _ = await ctx.get_translate_function()
-        await ctx.send(_("I'm having a issue with finding the country name here! Here's what I'm "
-                         "showing: {trying}",
-                         trying=await ctx.bot.worldometers_api.try_to_get_name(country)))
+        await ctx.send(_("I'm having a issue with finding the country name here! Here's what I'm showing: {0}",
+                         await ctx.bot.worldometers_api.try_to_get_name(name)))
+        return None
+    else:
+        return embed
 
 
+async def world(ctx: MyContext):
+    return await continent(ctx, "world")
+
+
+country = state = continent
