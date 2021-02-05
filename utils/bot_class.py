00d22626d8a9526133eb8608c616c91fd1dd10af
0/0 <beerights@protonmail.com>
2021-02-04 19:28:06 -0700

    typos result in fatal errors

--- utils/bot_class.py
+++ utils/bot_class.py
@@ -124,9 +124,9 @@ class MyBot(AutoShardedBot):
             await self._worldometers_api.update_covid_19_virus_stats()
             await self._vaccine_api.update_covid_19_vaccine_stats()
             await self._jhucsse_api.update_covid_19_virus_stats()
-            await self.owid_api.update_covid_19_owid_data()
+            await self._owid_api.update_covid_19_owid_data()
         except RuntimeError as e:
-            self.logger.exception("Fatal RuntimeError while running inital update!", exception_instance=e)
+            self.logger.exception("Fatal RuntimeError while running initial update!", exception_instance=e)
         except Exception as e:
             self.logger.exception("Fatal general error while running initial update!", exception_instance=e)
         try:
