e1929567a459dba6534d341ce246ed8419e931d4
0/0 <beerights@protonmail.com>
2021-02-04 19:07:37 -0700

    add more info to the loggers

--- utils/logger.py
+++ utils/logger.py
@@ -1,14 +1,28 @@
 import logging
 import typing
+from datetime import datetime
 from pythonjsonlogger import jsonlogger
 import discord
+
+
+class CustomJsonFormatter(jsonlogger.JsonFormatter):
+    def add_fields(self, log_record, record, message_dict):
+        super(CustomJsonFormatter, self).add_fields(log_record, record, message_dict)
+        if not log_record.get('timestamp'):
+            # this doesn't use record.created, so it is slightly off
+            now = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+            log_record['timestamp'] = now
+        if log_record.get('level'):
+            log_record['level'] = log_record['level'].upper()
+        else:
+            log_record['level'] = record.levelname
 
 
 def init_logger() -> logging.Logger:
     # Set root logger to log to file as JSON
     logger = logging.getLogger()
     log_handler = logging.FileHandler(filename='/tmp/covid_bot/log1.json')
-    formatter = jsonlogger.JsonFormatter()
+    formatter = CustomJsonFormatter('%(timestamp)s %(level)s %(name)s %(message)s')
     log_handler.setFormatter(formatter)
     logger.addHandler(log_handler)
 
